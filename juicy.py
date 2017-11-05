2e82fe1e7738055e9dc0acf03bb966a6e1ae4a39
David W Grossman <2gotgrossman@gmail.com>
2017-11-05 00:59:51 -0400

    first commit of juicy: a module to share files over 100MB via GitHub

--- juicy.py
+++ juicy.py
@@ -3,22 +3,24 @@ from sys import getsizeof
 from math import ceil
 import os
 
-ONE_HUNDRED_MB = 104857600
+ONE_HUNDRED_MB = 100000000 # Rounded down for smaller file sizes
 
 def to_file(json_data, filename):
-    json_string = dumps(json_data)
+    json_string = str(dumps(json_data))
 
-    x = getsizeof(getsizeof(json_string))
+    x = getsizeof(json_string)
     num_files = ceil(x / ONE_HUNDRED_MB)
+
+
     if num_files == 1:
-        with open(filename, "wb") as f:
+        with open(filename, "w") as f:
             f.write(json_string)
     else:
-        if not os.path.exists(filename):
+        if not os.path.isdir(filename):
             os.makedirs(filename)
         chars_per_file = len(json_string) // num_files
-        for i, file_no in enumerate(num_files):
-            with open(str(i) + ".juicy", "wb") as f:
+        for i in range(num_files):
+            with open(os.path.join(filename, str(i) + ".juicy"), "w") as f:
                 if i == num_files - 1:
                     f.write(json_string[i * chars_per_file : ])
                 else:
@@ -27,15 +29,15 @@ def to_file(json_data, filename):
 def from_file(filename):
     if not os.path.isdir(filename):
         with open(filename, "rb") as f:
-             return loads(f.read())
+             return loads(f.readline())
     else:
-        files_to_join = []
+        imported_string = ""
         files = os.listdir(filename)
         num_juicy_files = len( list( filter( lambda x : ".juicy" in x, files) ) )
         for i in range(num_juicy_files):
-            with open(str(i) + ".juicy", "rb") as f:
-                files_to_join.append(f.read())
-
-        return ''.join(files_to_join)
+            with open(os.path.join(filename, str(i) + ".juicy"), "r") as f:
+                imported_string += f.read()
+        print("done with read")
+        return loads(imported_string)
 
 
